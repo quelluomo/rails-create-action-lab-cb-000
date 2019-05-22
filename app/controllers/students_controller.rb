2b9099642965982d1127bae9b4f166d1fd24a0bb
Matteo Muscas <thatman.j@gmail.com>
2019-05-22 10:05:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = "First Name"
     @student.last_name = "Last Name"
-    @student.save 
+    @student.save
     redirect_to student_path(@student)
   end
 
