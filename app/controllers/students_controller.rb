663e02b0696e6b24acd11fd5db391772a21156e4
Matteo Muscas <thatman.j@gmail.com>
2019-05-22 10:05:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new
+    @student.first_name = "First Name"
+    @student.last_name = "Last Name"
+    @student.save 
+    redirect_to student_path(@student)
   end
 
 end
