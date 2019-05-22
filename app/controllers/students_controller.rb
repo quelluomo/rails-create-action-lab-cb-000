a5ba45408be874b61abcf39c682642d7ee44fd0d
Matteo Muscas <thatman.j@gmail.com>
2019-05-22 10:18:30 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,8 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @student.first_name = "First Name"
-    @student.last_name = "Last Name"
+    @student.first_name = params[student][first_name]
+    @student.last_name = params[student][last_name]
     @student.save
     redirect_to student_path(@student)
   end
